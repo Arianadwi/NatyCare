60db3c408a96878a1391e5384601d1c3b25e23ea
Putri Cantika Salsabilla <cucuorangkaya@gmail.com>
2026-04-17 22:48:24 +0700

    menambahkan endpoint produk

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProdukController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/produk', [ProdukController::class, 'index']);
+Route::post('/produk', [ProdukController::class, 'store']);
+Route::get('/produk/{id}', [ProdukController::class, 'show']);
+Route::put('/produk/{id}', [ProdukController::class, 'update']);
+Route::delete('/produk/{id}', [ProdukController::class, 'destroy']);
