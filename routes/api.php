10cbc609d5422de39530143f4f180594345439c6
Putri Cantika Salsabilla <cucuorangkaya@gmail.com>
2026-05-10 21:26:55 +0700

    update background pada katalog

--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,15 @@ use App\Http\Controllers\ProdukController;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
+/* PRODUK BISA DIAKSES TANPA LOGIN */
+Route::get('/produk', [ProdukController::class, 'index']);
+Route::get('/produk/{id}', [ProdukController::class, 'show']);
+
+/* YANG PERLU LOGIN */
 Route::middleware('auth:sanctum')->group(function () {
+
     Route::post('/produk', [ProdukController::class, 'store']);
-    Route::get('/produk/{id}', [ProdukController::class, 'show']);
     Route::put('/produk/{id}', [ProdukController::class, 'update']);
     Route::delete('/produk/{id}', [ProdukController::class, 'destroy']);
-});
 
-// TANPA AUTH
-Route::get('/produk', [ProdukController::class, 'index']);
+});
