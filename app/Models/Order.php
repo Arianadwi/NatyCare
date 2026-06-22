1f46446f3496d339e5222064c7451b65c7286b02
Putri Cantika Salsabilla <cucuorangkaya@gmail.com>
2026-06-22 20:20:11 +0700

    [BE] Update Detail Pesanan

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,12 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Order extends Model
 {
     protected $table = 'orders';
 
     protected $fillable = [
+        'user_id',
+        'nama_lengkap',
+        'no_whatsapp',
+        'alamat_lengkap',
+        'provinsi',
+        'kota',
+        'kecamatan',
+        'kode_pos',
+        'catatan',
         'metode_pembayaran',
         'metode_pengiriman',
         'ongkir',
@@ -21,6 +31,11 @@ class Order extends Model
     public function items()
     {
         return $this->hasMany(OrderItem::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 
 }
